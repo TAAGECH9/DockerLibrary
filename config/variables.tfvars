085849130b88329255d439c63720c2d505a8e791
TAAGECH9 <christopher.germann@swisscom.com>
2022-10-16 16:06:27 +0200

    Update var + delete unnecessary file

---
+++ config/variables.tfvars
@@ -0,0 +1,12 @@
+
+
+"docker-rg" = {
+  location = "West Europe"
+  name     = "docker-image-library-rg"
+}
+
+"acr" = {
+  name          = "docker-image-library"
+  sku          = "Basic"
+  location = "West Europe"
+}
--- iac/variables.tf
+++ iac/variables.tf
@@ -1,4 +1,14 @@
+# TF Cloud Variables
 variable "client_id" {}
 variable "client_secret" {}
 variable "subscription_id" {}
 variable "tenant_id" {}
+
+
+variable "docker-rg" {
+    type = map(string)
+}
+
+variable "acr" {
+    type = map(string)
+}
