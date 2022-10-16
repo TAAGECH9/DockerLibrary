868fd57bb6f71b4d11a9d3daeb2d096e1a9aaac1
TAAGECH9 <christopher.germann@swisscom.com>
2022-10-16 16:19:33 +0200

    hotfix

--- iac/acr.tf
+++ iac/acr.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_registry" "acr" {
   name                = var.acr.name
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.acr_rg.name
   location            = var.acr.location
   sku                 = var.acr.sku
 }
