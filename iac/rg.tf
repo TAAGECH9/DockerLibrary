137c286c2e8a77f69d6acbca82dffe408d2b6842
TAAGECH9 <christopher.germann@swisscom.com>
2022-10-16 16:09:36 +0200

    Correctly accessing the terraform variables

--- iac/rg.tf
+++ iac/rg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "example" {
-  name     = var.docker-rg.name
-  location = var.docker-rg.location
+  name     = var.docker-rg["name"]
+  location = var.docker-rg["location"]
 }
