ea1b022a08aa6e3a262c0bf14bb1c1c438d11669
TAAGECH9 <christopher.germann@swisscom.com>
2022-11-09 15:56:07 +0100

    removing subscription id

--- terraform/iac/main.tf
+++ terraform/iac/main.tf
@@ -21,7 +21,6 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-  subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
