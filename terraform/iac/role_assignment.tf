79302af8fa2e1e6bacc58c630f72c0d7f6ba2917
TAAGECH9 <christopher.germann@swisscom.com>
2022-11-10 10:15:50 +0100

    Using object id

--- terraform/iac/role_assignment.tf
+++ terraform/iac/role_assignment.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "test_role_assignment" {
-  principal_id                     = data.azuread_application.docker-image-library.id
+  principal_id                     = data.azuread_application.docker-image-library.object_id
   role_definition_name             = "AcrPush"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
