1ee88ab2b8c2edad567ee7801aea27e9012d19d0
TAAGECH9 <christopher.germann@swisscom.com>
2022-11-10 10:10:55 +0100

    Tryout

--- terraform/iac/data_sources.tf
+++ terraform/iac/data_sources.tf
@@ -1,3 +1,7 @@
 data "azuread_group" "test" {
   display_name = "test"
 }
+
+data "azuread_application" "docker-image-library" {
+  display_name = "docker-image-library"
+}
--- terraform/iac/role_assignment.tf
+++ terraform/iac/role_assignment.tf
@@ -1,6 +1,6 @@
-resource "azurerm_role_assignment" "acr_pull_role" {
-  principal_id                     = data.azuread_group.test.object_id
-  role_definition_name             = "Contributor"
+resource "azurerm_role_assignment" "test_role_assignment" {
+  principal_id                     = data.azuread_application.docker-image-library.id
+  role_definition_name             = "AcrPush"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
 }
